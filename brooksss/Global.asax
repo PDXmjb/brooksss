1066cd44451727c3418ea79edc2212f54d8f09b6
Mike Brooks <Mike Brooks@DESKTOP-GPKBUE2>
2017-08-14 12:19:39 -0700

    Add project files.

---
+++ brooksss/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="brooksss.MvcApplication" Language="C#" %>
---
+++ brooksss/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(brooksss.Startup))]
+namespace brooksss
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
